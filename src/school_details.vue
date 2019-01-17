93d9480e197fbfed2e1a83e56caf0cee69d47242
maryse <gwizamaryse@gmail.com>
2019-01-17 15:09:00 +0900

    Removing javascripts console errors

--- src/school_details.vue
+++ src/school_details.vue
@@ -503,10 +503,10 @@
             <p class="text-xs-center display-1">Contact Us</p>
           </v-card-text>
           <v-layout align-center justify-center>
-            <v-form ref="form" v-model="valid" lazy-validation>
-              <v-text-field v-model="name" :rules="nameRules" :counter="10" label="Name" required></v-text-field>
-              <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
-              <v-text-field v-model="email" :rules="emailRules" label="Telephone" required></v-text-field>
+            <v-form ref="form" v-model="" lazy-validation>
+              <v-text-field v-model="" :rules="" :counter="10" label="Name" required></v-text-field>
+              <v-text-field v-model="" :rules="" label="E-mail" required></v-text-field>
+              <v-text-field v-model="" :rules="" label="Telephone" required></v-text-field>
               <v-textarea label="Comments / Questions" required outline solo light></v-textarea>
               <v-btn :disabled="!valid" @click="submit">submit</v-btn>
             </v-form>
